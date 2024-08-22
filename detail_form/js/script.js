5da33a832e98eea7d826ab90870062ecb7f32c31
mshan <mshankc@gmail.com>
2024-08-22 14:19:24 +0530

    initiated validation

---
+++ detail_form/js/script.js
@@ -0,0 +1,11 @@
+function validation(){
+    const name=document.getElementById('name').nodeValue;
+    const number=document.getElementById('name').nodeValue;
+    const email=document.getElementById('name').nodeValue;
+    const age=document.getElementById('age').nodeValue;
+
+    document.write(name);
+    document.write(number);
+    document.write(email);
+    document.write(age);
+}
